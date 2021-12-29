13fc26e12a51c82fbb12fddb0767d17bbd76e343
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2021-12-29 13:50:59 +0100

    Changement dans la bdd

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ use db;
 
 create table administrateur
 (
-    AdminId int         not null primary key AUTO_INCREMENT,
+    AdminId int          not null primary key AUTO_INCREMENT,
     Nom     varchar(255) not null,
     Email   varchar(255) not null,
     Tel     varchar(255) not null
@@ -10,7 +10,7 @@ create table administrateur
 
 create table gestionnaire
 (
-    GestId int(45)     not null primary key AUTO_INCREMENT,
+    GestId int(45)      not null primary key AUTO_INCREMENT,
     RIB    varchar(255) not null,
     Nom    varchar(255) not null,
     Prenom varchar(255) not null,
@@ -20,33 +20,35 @@ create table gestionnaire
 
 create table groupe
 (
-    GroupId    int(45)     not null primary key AUTO_INCREMENT,
+    GroupeId   int(45)      not null primary key AUTO_INCREMENT,
+    NomGroupe  varchar(255) not null,
     SIRET      varchar(255) not null,
     Secteur    varchar(255) not null,
     Adresse    varchar(255) not null,
-    Classement int(45)     not null,
-    Gestid     int(45)     null,
+    Classement int(45)      not null,
+    Code       varchar(255) not null,
+    GestId     int(45)      null,
     constraint groupe_gestionnaire_GestId_fk
-        foreign key (Gestid) references gestionnaire (GestId)
+        foreign key (GestId) references gestionnaire (GestId)
             on update cascade on delete cascade
 );
 
 create table membre
 (
-    MembreId int         not null primary key AUTO_INCREMENT,
-    Nom      varchar(255)         not null,
-    Prenom   varchar(255)         not null,
-    Email    varchar(255)         not null,
+    MembreId int          not null primary key AUTO_INCREMENT,
+    Nom      varchar(255) not null,
+    Prenom   varchar(255) not null,
+    Email    varchar(255) not null,
     Mdp      varchar(255) not null,
-    GroupId  int         not null,
-    GestId   int(45)     null,
-    constraint membre_groupe_Gestid_fk
-        foreign key (GestId) references groupe (Gestid),
+    GroupeId int          not null,
+    GestId   int(45)      null,
+    constraint membre_groupe_GestId_fk
+        foreign key (GestId) references groupe (GestId),
     constraint membre_ibfk_1
-        foreign key (GroupId) references groupe (GroupId)
+        foreign key (GroupeId) references groupe (GroupeId)
             on update cascade on delete cascade
 );
 
 create index ForeignGroup
-    on membre (GroupId);
+    on membre (GroupeId);
 

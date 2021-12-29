6c965c3299e9cb35411ad4932a48fe55916e01ae
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2021-12-29 13:30:01 +0100

    Afficher informations de profils sur page de profil

--- PHP/profilGest.php
+++ PHP/profilGest.php
@@ -1,3 +1,24 @@
+<?php
+include "connect_to_db.php";
+$db = dbConnect();
+
+session_start();
+
+$reponsesGest = $db->prepare('SELECT GestId, RIB, Nom, Prenom, Email FROM gestionnaire WHERE GestId = :gestid');
+$reponsesGest->bindParam('gestid', $_SESSION['GestId']);
+
+$reponsesGroupe = $db->prepare('SELECT GroupeId, NomGroupe, SIRET, Secteur, Adresse, Code, GestId FROM groupe WHERE GestId = :gestid');
+$reponsesGroupe->bindParam('gestid', $_SESSION['GestId']);
+
+$reponsesGest->execute();
+$reponsesGroupe->execute();
+
+$donneesGest = $reponsesGest->fetch();
+$donneesGroupe = $reponsesGroupe->fetch();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +26,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/nav.css">
+    <link rel="stylesheet" href="../CSS/background.css">
     <title>Document</title>
 </head>
 
-<body>
-    <p>Profil gestionnaire</p>
+<body class="background">
+    <?php
+    include "navaffichage.php";
+    navaffichage();
+    ?>
+
+    <h1>Profil gestionnaire</h1>
+    <h2>Informations personnelles gestionnaire</h2>
+    <p><?php echo $donneesGest['Nom'] ?></p>
+    <p><?php echo $donneesGest['Prenom'] ?></p>
+    <p><?php echo $donneesGest['Email'] ?></p>
+    <p><?php echo $donneesGest['RIB'] ?></p>
+
+
+    <h2>Informations du groupe</h2>
+    <p><?php echo $donneesGroupe['NomGroupe'] ?></p>
+    <p><?php echo $donneesGroupe['SIRET'] ?></p>
+    <p><?php echo $donneesGroupe['Secteur'] ?></p>
+    <p><?php echo $donneesGroupe['Adresse'] ?></p>
+    <p><?php echo $donneesGroupe['Code'] ?></p>
 </body>
 
 </html>
--- PHP/profilMembre.php
+++ PHP/profilMembre.php
@@ -1,3 +1,22 @@
+<?php
+include "connect_to_db.php";
+$db = dbConnect();
+
+session_start();
+
+$reponsesMembre = $db->prepare('SELECT Nom, Prenom, Email FROM membre WHERE MembreId = :membreid');
+$reponsesMembre->bindParam('membreid', $_SESSION['MembreId']);
+
+$reponsesGroupe = $db->prepare('SELECT NomGroupe, SIRET, Secteur, Adresse, Code FROM groupe JOIN membre ON groupe.GroupeId = membre.GroupeId AND membre.MembreId = :membreid');
+$reponsesGroupe->bindParam('membreid', $_SESSION['MembreId']);
+
+$reponsesMembre->execute();
+$reponsesGroupe->execute();
+
+$donneesMembre = $reponsesMembre->fetch();
+$donneesGroupe = $reponsesGroupe->fetch();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +24,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/nav.css">
+    <link rel="stylesheet" href="../CSS/background.css">
     <title>Document</title>
 </head>
 
-<body>
-    <p>Profil membre</p>
+<body class="background">
+    <?php
+    include "navaffichage.php";
+    navaffichage();
+    ?>
+
+    <h1>Profil membre</h1>
+    <h2>Informations personnelles gestionnaire</h2>
+    <p><?php echo $donneesMembre['Nom'] ?></p>
+    <p><?php echo $donneesMembre['Prenom'] ?></p>
+    <p><?php echo $donneesMembre['Email'] ?></p>
+
+    <h2>Informations du groupe</h2>
+    <p><?php echo $donneesGroupe['NomGroupe'] ?></p>
+    <p><?php echo $donneesGroupe['SIRET'] ?></p>
+    <p><?php echo $donneesGroupe['Secteur'] ?></p>
+    <p><?php echo $donneesGroupe['Adresse'] ?></p>
+    <p><?php echo $donneesGroupe['Code'] ?></p>
 </body>
 
 </html>

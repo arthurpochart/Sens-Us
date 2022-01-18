81b48dff95358f75bf047b06143e1afc84bee04d
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2022-01-18 11:56:53 +0100

    correction style nos réseaux

--- Vue/nosReseaux_vue.php
+++ Vue/nosReseaux_vue.php
@@ -19,47 +19,46 @@
     include "../Controleur/nav_controleur.php";
     nav_controleur();
     ?>
-    <header class="header">
-        <h1>Nous contacter sur nos réseaux!</h1>
-    </header>
     <div id="all-page">
-        <div>
-            <div id="container-all-person">
-                <div class="container-one-person">
-                    <a href="https://www.facebook.com/SensUs-113391014559152" target="_blank">
-                        <img src="../Assets/Images/ICONCRAZE-COM-Facebook-Icon-PNG.png" width="200" alt="Facebook" />
-                    </a>
-                    <h1>
-                        Rejoignez-nous sur Facebook !    
-                    </h1>
-                    <p>Venez consulter et aimer notre page Sens’Us sur Facebook ! </br> 
-Vous y trouverez différentes informations sur notre, entreprise, des photos exclusives mais pourrez également prendre part aux différents événements proposés par Sens’Us !
-                    </p>
-                </div>
-                <div class="container-one-person">
-                    <a href="https://linkedin.com/" target="_blank">
-                        <img src="../Assets/Images/linkedin-icon2.png" width="200" alt="Linkedin" />
-                    </a>
-                    <h1>
-                        Rejoignez-nous sur Linkedin !
-                    </h1>
-                    <p>Suivez le profil Linkedin de Sens’Us ! </br>
-Afin de découvrir plus en profondeur notre identité, nos collaborateurs, nos employés, n’hésitez pas à consulter notre profil Linkedin ! Régulièrement alimenté, vous y trouverez des résumés de conférence, des informations sur nos projets en cours et à venir et tout une collection d’informations sur notre groupe !
+        <div id="presentation-generale">
+            <h1>Nous contacter sur nos réseaux</h1>
+        </div>
+        <div id="container-all-person">
+            <div class="container-one-person">
+                <a href="https://www.facebook.com/SensUs-113391014559152" target="_blank">
+                    <img src="../Assets/Images/ICONCRAZE-COM-Facebook-Icon-PNG.png" width="150" alt="Facebook" />
+                </a>
+                <h1>
+                    Rejoignez-nous sur Facebook !
+                </h1>
+                <p>Venez consulter et aimer notre page Sens’Us sur Facebook ! </br>
+                    Vous y trouverez différentes informations sur notre, entreprise, des photos exclusives mais pourrez également prendre part aux différents événements proposés par Sens’Us !
+                </p>
+            </div>
+            <div class="container-one-person">
+                <a href="https://linkedin.com/" target="_blank">
+                    <img src="../Assets/Images/linkedin-icon2.png" width="150" alt="Linkedin" />
+                </a>
+                <h1>
+                    Rejoignez-nous sur Linkedin !
+                </h1>
+                <p>Suivez le profil Linkedin de Sens’Us ! </br>
+                    Afin de découvrir plus en profondeur notre identité, nos collaborateurs, nos employés, n’hésitez pas à consulter notre profil Linkedin ! Régulièrement alimenté, vous y trouverez des résumés de conférence, des informations sur nos projets en cours et à venir et tout une collection d’informations sur notre groupe !
 
-                    </p>
-                </div>
-                <div class="container-one-person">
-                    <a href="https://www.Instagram.com/" target="_blank">
-                        <img src="../Assets/Images/insta-logo2.png" width="200" alt="Instagram" />
-                    </a>
-                    <h1>
-                        Rejoignez-nous sur Instagram ! 
-                    </h1>
-                    <p>Sens’Us arrive sur Instagram ! </br>
-Après Facebook et Linkedin, Sens’Us arrive finalement sur le réseau social le plus hype du moment ! Venez suivre notre profil, et bénéficiez de photos et de stories exceptionnelles. Vous pourrez constater le déroulement de nos événements, le développement de nos équipes et aurez accès de manière privilégiée à nos différents projets !
-                    </p>
-                </div>
+                </p>
+            </div>
+            <div class="container-one-person">
+                <a href="https://www.Instagram.com/" target="_blank">
+                    <img src="../Assets/Images/insta-logo2.png" width="150" alt="Instagram" />
+                </a>
+                <h1>
+                    Rejoignez-nous sur Instagram !
+                </h1>
+                <p>Sens’Us arrive sur Instagram ! </br>
+                    Après Facebook et Linkedin, Sens’Us arrive finalement sur le réseau social le plus hype du moment ! Venez suivre notre profil, et bénéficiez de photos et de stories exceptionnelles. Vous pourrez constater le déroulement de nos événements, le développement de nos équipes et aurez accès de manière privilégiée à nos différents projets !
+                </p>
             </div>
         </div>
+    </div>
     </div>
 </body>

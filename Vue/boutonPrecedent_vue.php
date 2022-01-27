1fb27bc71d8ca93b7ff55a32058b658a75fd3fdb
Julien Dorffer <julien.drf@gmail.com>
2022-01-27 18:01:41 +0100

    page précédente

--- Vue/boutonPrecedent_vue.php
+++ Vue/boutonPrecedent_vue.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Merci à vous !</title>
+    <title>Erreur + redirection</title>
     <link rel="stylesheet" href="../CSS/remerciements.css">
 </head>
 

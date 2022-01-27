4b6a914f1676b69cbfa7ebd79bca8d2f78118d42
Julien Dorffer <julien.drf@gmail.com>
2022-01-27 18:02:04 +0100

    Page de contact

--- Vue/contact_vue.php
+++ Vue/contact_vue.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Nous contacter</title>
+  <title>Page de contact</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../CSS/contact.css">
   <link rel="stylesheet" href="../CSS/nav.css">

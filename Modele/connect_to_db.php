4e625e116f6c1ac3a2ee09c45f6bbb653bbb2faf
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2022-06-13 09:43:59 +0200

    changement id bdd

--- Modele/connect_to_db.php
+++ Modele/connect_to_db.php
@@ -3,7 +3,7 @@
 function dbConnect()
 {
     try {
-        $db = new PDO('mysql:host=localhost:3306;dbname=sensus;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=herogu.garageisep.com;dbname=00p4tnpaKt_sensus;charset=utf8', 'jW5KoIfz8t_sensus', 'flbvkeHjeXIQwgMG');
         return $db;
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());

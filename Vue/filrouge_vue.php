908e8f4b7f69078b741c4cb3e223cafe81a63842
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2022-06-17 16:26:25 +0200

    allllllller

--- Vue/filrouge_vue.php
+++ Vue/filrouge_vue.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../CSS/FAQ.css">
     <link rel="stylesheet" href="../CSS/nav.css">
     <link rel="stylesheet" href="../CSS/background.css">
-    <title>Page de connexion</title>
+    <title>Fil rouge</title>
 </head>
 
 <body class="background">
     <?php
     include "Controleur/recuperer_trame_controleur.php";
     ?>
-
-
 </body>
 
 </html>

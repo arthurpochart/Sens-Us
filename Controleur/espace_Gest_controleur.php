56d736d44626b1fb4fa12096b81d20d9f3beddb0
Gauthier Itart-Longueville <gauthier-il@hotmail.fr>
2022-06-23 11:06:50 +0200

    ajustement style

--- Controleur/espace_Gest_controleur.php
+++ Controleur/espace_Gest_controleur.php
@@ -8,4 +8,17 @@ require("../Modele/recuperer_trame_modele.php");
 
 $resultatTrame = recuperer_trame_modele($db);
 
+function compareByTimeStamp($time1, $time2)
+{
+    if (strtotime($time1["Horodatage"]) < strtotime($time2["Horodatage"]))
+        return 1;
+    else if (strtotime($time1["Horodatage"]) > strtotime($time2["Horodatage"]))
+        return -1;
+    else
+        return 0;
+}
+
+// sort array with given user-defined function
+usort($resultatTrame, "compareByTimeStamp");
+
 require("../Vue/espace_Gest_vue.php");
